f5990250cef06362e81133ec87143686bc8f98e0
hasita-art <73806182+hasita-art@users.noreply.github.com>
2022-05-15 18:05:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let camera=document.getElementById("camera");
-let gesture_name="";
-let emoji="";
+let emoji1="";
+let emoji2="";
+let emoji3="";
+let emoji4="";
+let emoji5="";
 
 Webcam.set({
     width: 350,
@@ -9,10 +12,38 @@ Webcam.set({
     png_quality: 90,
 });
 
-webcam.attach('#camera');
+Webcam.attach('#camera');
 
 function take_pic(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML="";
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
+console.log('ml5 version',ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/IED6sF3Oz/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("model loaded")
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1="This emoji is "
+
+};
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+
